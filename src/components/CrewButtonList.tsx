10e498da01cae9a9ee05a032aafb26acce75e19d
Mapelujo <olamideiyanda18@gmail.com>
2024-08-29 00:58:35 +0100

    done with the basic functionality of the crew page

--- src/components/CrewButtonList.tsx
+++ src/components/CrewButtonList.tsx
@@ -1,16 +1,32 @@
-import { CrewMember } from "./CrewDetails"
+import { CrewMember } from "./CrewDetails";
 
 type CrewButtonListProps = {
-    crew: CrewMember[],
+  crew: CrewMember[];
+  displayCrew: number;
+  crewSwitch: (index: number) => void;
+};
+
+export default function CrewButtonList({
+  crew,
+  displayCrew,
+  crewSwitch,
+}: CrewButtonListProps) {
+  return crew.map((_, index) => (
+    <button
+      onClick={() => crewSwitch(index)}
+      key={index}
+      className={
+        index === displayCrew
+          ? "bg-white h-[2rem] w-[2rem] rounded-full cursor-pointer"
+          : "bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full cursor-pointer"
+      }
+    ></button>
+  ));
 }
 
-export default function CrewButtonList({crew}: CrewButtonListProps) {
-  return (
-    crew.map((crewMember, index) => <button key={index} className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button> )
-  )
+{
+  /* <button className="bg-white opacity-[0.174363] active-pagination-button h-[2rem] w-[2rem] rounded-full"></button>
+<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button>
+<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button>
+<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button> */
 }
-
-{/* <button className="bg-white opacity-[0.174363] active-pagination-button h-[2rem] w-[2rem] rounded-full"></button>
-<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button>
-<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button>
-<button className="bg-white opacity-[0.174363] h-[2rem] w-[2rem] rounded-full"></button> */}
--- src/components/CrewDetails.tsx
+++ src/components/CrewDetails.tsx
@@ -1,78 +1,96 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useReducer } from "react";
 import CrewButtonList from "./CrewButtonList";
 
 export interface CrewMember {
-    name: string;
-    images: {png: string};
-    role: string;
-    bio: string;
+  name: string;
+  images: { png: string };
+  role: string;
+  bio: string;
 }
 
 type State = {
-    crew: CrewMember[],
-    displayCrew: number
-}
+  crew: CrewMember[];
+  displayCrew: number;
+};
 
 const initialState: State = {
-    crew: [],
-    displayCrew: 0
-}
+  crew: [],
+  displayCrew: 0,
+};
 
 function reducer(state, action) {
-    switch (action.type) {
-        case 'crew/fetching':
-            return {
-                ...state,
-                crew: action.payload
-            }
-    }
+  switch (action.type) {
+    case "crew/fetching":
+      return {
+        ...state,
+        crew: action.payload,
+      };
+    case "crew/switching":
+      return {
+        ...state,
+        displayCrew: action.payload,
+      };
+  }
 }
 
-
 export default function CrewDetails() {
-    const [{crew, displayCrew} , dispatch] = useReducer(reducer, initialState); // State to hold crew data
-    const selectedCrew = crew[displayCrew];
+  const [{ crew, displayCrew }, dispatch] = useReducer(reducer, initialState); // State to hold crew data
+  const selectedCrew = crew[displayCrew];
 
-    useEffect(function() {
-        async function fetchCrew() {
-            try {
-                const res = await fetch('/data.json');
-                const data = await res.json();
-                dispatch({type: "crew/fetching", payload: data.crew})
-            } catch (err) {
-                console.error(err);
-            }
-            
-        }
+  useEffect(function () {
+    async function fetchCrew() {
+      try {
+        const res = await fetch("/data.json");
+        const data = await res.json();
+        dispatch({ type: "crew/fetching", payload: data.crew });
+      } catch (err) {
+        console.error(err);
+      }
+    }
 
-        fetchCrew();
-    }, [])
+    fetchCrew();
+  }, []);
 
-    console.log(selectedCrew);
-    
+  console.log(selectedCrew);
 
+  function crewSwitch(index: number) {
+    dispatch({ type: "crew/switching", payload: index });
+  }
 
   return (
-    <main className='relative text-white flex w-[75%] mx-auto justify-between gap-spacing-400 items-center py-[1.5rem]'>
-        <div className='flex flex-col gap-spacing-300 basis-[49%]'>
-            <article className='flex flex-col'>
-                <h3 className='font-bellefair text-[3.2rem] uppercase text-white opacity-[0.5042]'>{selectedCrew?.role}</h3>
-                <p className='font-barlow text-[5.6rem] text-white mb-spacing-300 mt-spacing-200 uppercase'>
-                    {selectedCrew?.name}
-                </p>
-                <p className='font-barlow text-[1.8rem] text-blue-300 leading-[3.24rem]'>
-                    {selectedCrew?.bio}
-                </p>
-            </article>
+    <main className="relative text-white flex w-[75%] mx-auto justify-between gap-spacing-400 items-center py-[1.5rem]">
+      <div className="flex flex-col gap-spacing-600 basis-[49%]">
+        <article className="flex flex-col">
+          <h3 className="font-bellefair text-[3.2rem] uppercase text-white opacity-[0.5042]">
+            {selectedCrew?.role}
+          </h3>
+          <p className="font-barlow text-[5.6rem] text-white mb-spacing-300 mt-spacing-200 uppercase">
+            {selectedCrew?.name}
+          </p>
+          <p className="font-barlow text-[1.8rem] text-blue-300 leading-[3.24rem]">
+            {selectedCrew?.bio}
+          </p>
+        </article>
+        <div className="flex gap-spacing-500">
+          <CrewButtonList
+            crew={crew}
+            displayCrew={displayCrew}
+            crewSwitch={crewSwitch}
+          />
         </div>
-        <div className='w-[40%] '>
-            <img className='w-full ' src='/assets/crew/image-douglas-hurley.png' alt='Douglas-image' />
-        </div>
+      </div>
+      <div className="w-[40%] ">
+        <img
+          className="w-full "
+          src={selectedCrew?.images?.png}
+          alt="Douglas-image"
+        />
+      </div>
 
-        {/* Pagination buttons */}
-        <div className='absolute flex gap-spacing-500 top-[55rem]'>
-            <CrewButtonList crew={crew} />
-        </div>
+      {/* Pagination buttons */}
+      {/* <div className='absolute flex gap-spacing-500'>
+            <CrewButtonList crew={crew} displayCrew={displayCrew} crewSwitch={crewSwitch}/>
+        </div> */}
     </main>
-  )
+  );
 }
