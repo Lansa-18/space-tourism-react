96732446de7f63c148ed170e92492fde7994be16
Mapelujo <olamideiyanda18@gmail.com>
2024-09-13 21:30:58 +0100

    working on the homepage responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,34 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function Main() {
   return (
-    <main>
-      <section className="text-white uppercase flex justify-between items-center w-[70%] mx-auto mt-[7rem]">
-        <div className="flex flex-col gap-[2.4rem] basis-[50%]">
-          <p className="text-blue-300 text-[2.8rem] tracking-[.4rem] font-barlow-condensed">
-            So, you want to travel to
-          </p>
-          <p className="text-[14.4rem] font-bellefair">Space</p>
-          <p className="text-blue-300 text-[1.8rem] font-barlow-condensed leading-[3.24rem]">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
-        <article>
-          {/* TRANSPARENT DIV THAT OVERLAYS UPON HOVERING*/}
-          {/* <div className="rounded-full bg-red-950 h-[43rem] w-[43rem] opacity-100 z-10 transition-all duration-150 hover:opacity-20"></div>
-          <div className="rounded-full absolute top-[34rem] ml-[8rem] -z-10">
-            <img
-              className="rounded-full"
-              src="/assets/home/explore.png"
-              alt="explore-image"
-            />
-          </div> */}
+    <main className="text-white uppercase flex justify-between items-center w-[70%] mx-auto mt-[7rem] tab-port:flex-col tab-port:gap-[6rem] ">
+      <div className="flex flex-col gap-[2.4rem] basis-[50%] tab-port:text-center">
+        <p className="text-blue-300 text-[2.8rem] tab-land:text-[2.4rem] tracking-[.4rem] tab-land:tracking-[.2rem] font-barlow-condensed">
+          So, you want to travel to
+        </p>
+        <p className="text-[14.4rem] tab-land:text-[12.5rem] font-bellefair">
+          Space
+        </p>
+        <p className="text-blue-300 text-[1.8rem] font-barlow-condensed leading-[3.24rem]">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
 
-          <Link to='/destination'>   
+      {/* AT DESKTOP SCREEN SIZE  */}
+      <article className="tab-land:hidden">
+        <Link to="/destination">
           <div className="relative">
-            <div className="rounded-full ml-[-7rem] mt-[-5rem] bg-white h-[45rem] w-[45rem] opacity-0 z-10 ease-in-out duration-500 hover:opacity-20 absolute inset-0"></div>
+            <div className="rounded-full ml-[-7rem] mt-[-5rem] bg-white h-[45rem] w-[45rem] tab-port: opacity-0 z-10 ease-in-out duration-500 hover:opacity-20 absolute inset-0"></div>
             <div className="rounded-full relative ml-[2rem] pt-[3.8rem]">
               <img
                 className="rounded-full"
@@ -38,9 +31,17 @@ export default function Main() {
               />
             </div>
           </div>
-          </Link>
-        </article>
-      </section>
+        </Link>
+      </article>
+
+      {/* TABLET AND MOBILE SCREENS */}
+      <article>
+        <Link to="/destination">
+          <div className="bg-white rounded-full p-[2rem] w-[35rem] h-[35rem] text-center flex justify-center items-center text-blue-900 text-[4rem] font-normal font-bellefair">
+            EXPLORE
+          </div>
+        </Link>
+      </article>
     </main>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 export default function Homepage() {
   return (
-    <div className='home-bg-desktop h-screen w-full'>
+    <div className='home-bg-desktop min-h-screen w-full'>
       <Navbar />
       <Main />
     </div>
