f0f8807a64949d63d006e7170c82782a5aa3c738
Mapelujo <olamideiyanda18@gmail.com>
2024-09-21 22:40:52 +0100

    done with homepage responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Main() {
   return (
-    <main className="text-white uppercase flex justify-between items-center w-[70%] mx-auto mt-[7rem] tab-port:flex-col tab-port:gap-[6rem] ">
+    <main className="text-white uppercase flex justify-between items-center w-[70%] mx-auto mt-[7rem] tab-port:flex-col tab-port:gap-[6rem]">
       <div className="flex flex-col gap-[2.4rem] basis-[50%] tab-port:text-center">
         <p className="text-blue-300 text-[2.8rem] tab-land:text-[2.4rem] tracking-[.4rem] tab-land:tracking-[.2rem] font-barlow-condensed">
           So, you want to travel to
         </p>
-        <p className="text-[14.4rem] tab-land:text-[12.5rem] font-bellefair">
+        <p className="text-[14.4rem] tab-land:text-[12.5rem] phone:text-[9.5rem] font-bellefair">
           Space
         </p>
         <p className="text-blue-300 text-[1.8rem] font-barlow-condensed leading-[3.24rem]">
--- src/components/NavOverlay.tsx
+++ src/components/NavOverlay.tsx
@@ -1,15 +1,71 @@
+import { NavLink } from "react-router-dom";
+
 type NavOverlayProps = {
   activeHamburger: boolean;
   setActiveHamburger: React.Dispatch<React.SetStateAction<boolean>>;
-}
+};
 
-export default function NavOverlay({ activeHamburger, setActiveHamburger }: NavOverlayProps) {
-  if (!activeHamburger) return null;  
+export default function NavOverlay({
+  activeHamburger,
+  setActiveHamburger,
+}: NavOverlayProps) {
+  if (!activeHamburger) return null;
   return (
-    <article className="min-h-[200vh] border border-red-900 absolute top-0 right-0 backdrop-blur-[4rem] z-50 w-[55%]">
-      <div onClick={() => setActiveHamburger(false)}>
-        <img src="/assets/shared/icon-close.svg" alt="close-icon" />
+    <article className="hidden custom-850:flex flex-col gap-spacing-600 min-h-[200vh] border border-red-900 absolute top-0 right-0 backdrop-blur-[4rem] z-50 w-[75%]">
+      <div className="ml-auto my-[4rem] pr-[4rem]" onClick={() => setActiveHamburger(false)}>
+        <img
+          className="w-[3rem] h-[3rem]"
+          src="/assets/shared/icon-close.svg"
+          alt="close-icon"
+        />
       </div>
+
+      <ul className="text-white flex flex-col gap-spacing-600 pl-[4rem]">
+        <li className="">
+          <NavLink
+            className="flex gap-spacing-150 tracking-[2px] text-[1.6rem]"
+            to="/"
+          >
+            <strong className="font-barlow-condensed font-bold text-[1.6rem] tracking-[2.7px]">
+              00
+            </strong>
+            HOME
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            className="flex gap-spacing-150 tracking-[2px] text-[1.6rem]"
+            to="/"
+          >
+            <strong className="">
+              01
+            </strong>
+            DESTINATION
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            className="flex gap-spacing-150 tracking-[2px] text-[1.6rem]"
+            to="/"
+          >
+            <strong className="">
+              02
+            </strong>
+            CREW
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            className="flex gap-spacing-150 tracking-[2px] text-[1.6rem]"
+            to="/"
+          >
+            <strong className="">
+              03
+            </strong>
+            TECHNOLOGY
+          </NavLink>
+        </li>
+      </ul>
     </article>
-  )
+  );
 }
