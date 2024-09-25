0db965c0cbc1b356971f5a9df5c58098ca7d4a3f
Mapelujo <olamideiyanda18@gmail.com>
2024-09-25 22:30:50 +0100

    making the logo route back to the homepage

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import LogoIcon from "/assets/shared/logo.svg";
 
 export default function Logo() {
   return (
-    <div className="flex ml-[6.4rem] gap-[6.4rem] items-center w-[50%] z-10 custom-850:ml-0">
+    <Link to='/' className="flex ml-[6.4rem] gap-[6.4rem] items-center w-[50%] z-10 custom-850:ml-0">
       <img src={LogoIcon} alt="logo-icon" />
       <div className="border-b border-white w-full opacity-[0.25] mr-[-3.2rem] tab-port:hidden"></div>
-    </div>
+    </Link>
   );
 }
